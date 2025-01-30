92cff06e8fde395bdcffead56833e778d1aa74ff
Bruno Araujo <brunnoaraujoc@gmail.com>
2025-01-30 15:16:06 -0300

    feat: adicionado conta Bancaria atraves do terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Por favor insira os dados da conta");
+        System.out.println("Digite o nome do cliente:");
+        String nome = scanner.nextLine();
+        System.out.println("Digite a agencia:");
+        String numeroAgencia = scanner.nextLine();
+        System.out.println("Digite o numero da conta:");
+        int numeroContaBanco = scanner.nextInt();
+        System.out.println("Digite o saldo da conta:");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia +
+                " e, conta " +  numeroContaBanco + " e seu saldo " + saldoConta + " já está disponível para saque");
+    }
 }
